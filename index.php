dc47494f8f7f90fc361d3f09915d55913860cb0b
Julia Werkoff <jwerkoff@gmail.com>
2016-05-30 11:17:13 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+        <link href="CSS/homepage.css" rel="stylesheet" media="all" type="text/css">
+        <title> Julia Werkoff - Porfolio</title>
+    </head>
+    <body>
+       <nav class="navbar navbar-inverse navbar-static-top">
+            <div class="container-fluid">
+               <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                      <span class="sr-only">Toggle navigation</span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Julia Werkoff</a>
+                </div>
+
+              <!-- Collect the nav links, forms, and other content for toggling -->
+              <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li><a href=#>Mon profil <span class="sr-only">(current)</span></a></li>
+                  <li><a href="#projects">Mes projets</a></li>
+                  <li><a href="#contact">Me contacter</a></li>
+                </ul> 
+                <div id='navbar-links' class="nav navbar-nav navbar-right">
+                    <a href="https://www.linkedin.com/pub/julia-werkoff/66/94b/154" target="_blank">
+                        <img alt="Brand" src="images/linkedin.png" height="35px">
+                    </a>
+                    <a href="https://github.com/Rouliita" target="_blank">
+                        <img alt="Brand" src="images/GitHub-Mark/PNG/GitHub-Mark-Light-32px.png" >
+                    </a>
+                </div>
+              </div><!-- /.navbar-collapse -->
+            </div><!-- /.container-fluid --> 
+           
+        </nav>  
+            
+        <div class="well-lg">
+            <div class="container-fluid">
+                <div class="row-fluid">
+                    <div class="col-md-3">
+                        <img src="images/juliap1.jpg" alt="photo perso" class="img-circle img-responsive">
+                    </div>
+                
+                    <div class="col-md-4" style='height:100%'>
+                        <div id="identity">
+                            <h2>Hello !</h2>
+                            <p class="lead">Je suis Julia Werkoff, manager de projets web freelance. blablablabalababalabzalbalabalbalabalabalbalabalbalab</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="well">
+            <div class="container-fluid">
+                <div class="row-fluid">              
+                    <div class="col-md-30" style='height:100%'>
+                        <div id="identity">
+                            <h1 id="projects">Mes projets</h1>
+                            <p class="lead">Je suis Julia Werkoff, manager de projets web freelance. blablablabalababalabzalbalabalbalabalabalbalabalbalab</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="well-lg">
+            <div class="container-fluid">
+                <div class="row-fluid">              
+                    <div class="col-md-30" style='height:100%'>
+                        <div id="identity">
+                            <h1 id="contact">Me contacter</h1>
+                               <form>
+                                    <div class="form-group">
+                                      <textarea></textarea> <br>
+                                    <button type="submit" class="btn btn-default">Envoyer</button>
+                              </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+            
+    </body>
+
+
+</html>
